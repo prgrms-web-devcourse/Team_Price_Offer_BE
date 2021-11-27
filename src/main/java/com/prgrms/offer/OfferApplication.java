30ac972aac919a42b836de5df5f7df3d5a3cd00c
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-27 21:57:48 +0900

    feat: spring boot 프로젝트 생성

---
+++ src/main/java/com/prgrms/offer/OfferApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/offer/OfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
