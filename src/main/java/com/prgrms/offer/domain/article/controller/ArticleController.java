2f4bd45d72616b14fc3f7a6720f47fe8d0c6a30a
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-06 14:43:27 +0900

    feat: 이미지 파일 -> S3 업로드 -> 이미지 URL 반환 API 추가

--- src/main/java/com/prgrms/offer/domain/article/controller/ArticleController.java
+++ src/main/java/com/prgrms/offer/domain/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.prgrms.offer.domain.article.controller;
 
 import com.prgrms.offer.common.ApiResponse;
 import com.prgrms.offer.common.message.ResponseMessage;
+import com.prgrms.offer.core.error.exception.BusinessException;
 import com.prgrms.offer.domain.article.model.dto.ArticleBriefViewResponse;
 import com.prgrms.offer.domain.article.model.dto.CategoriesResponse;
 import com.prgrms.offer.domain.article.service.ArticleService;
@@ -13,6 +14,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +28,24 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleController {
 
     private final ArticleService articleService;
+
+    @ApiOperation("이미지 -> URL 변환")
+    @PostMapping(value = "/imageUrls", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse> convertToImageUrls(@ModelAttribute List<MultipartFile> images) throws IOException {
+
+        if(images == null || images.isEmpty()){
+            throw new BusinessException(ResponseMessage.INVALID_IMAGE_EXCEPTION);
+        }
+
+        List<String> imageUrls = articleService.getImageUrls(images);
+
+        Map response = new HashMap<String, List<String>>();
+        response.put("imageUrls", imageUrls);
+
+        return ResponseEntity.ok(
+                ApiResponse.of(ResponseMessage.SUCCESS, response)
+        );
+    }
 
     @ApiOperation("게시글 전체 조회")
     @GetMapping()
--- src/main/java/com/prgrms/offer/domain/article/service/ArticleService.java
+++ src/main/java/com/prgrms/offer/domain/article/service/ArticleService.java
@@ -1,5 +1,6 @@
 package com.prgrms.offer.domain.article.service;
 
+import com.prgrms.offer.common.utils.S3ImageUploader;
 import com.prgrms.offer.domain.article.model.dto.ArticleBriefViewResponse;
 import com.prgrms.offer.domain.article.model.dto.CategoriesResponse;
 import com.prgrms.offer.domain.article.model.entity.Article;
@@ -9,12 +10,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final ArticleConverter converter;
+
+    private final S3ImageUploader s3ImageUploader;
+
+    private final String PRODUCT_IMAGE_DIR = "productImage";
 
     @Transactional(readOnly = true)
     public Page<ArticleBriefViewResponse> findAllByPages(Pageable pageable) {
@@ -25,5 +35,16 @@ public class ArticleService {
 
     public CategoriesResponse findAllCategories() {
         return converter.toCategoriesResponse();
+    }
+
+    public List<String> getImageUrls(List<MultipartFile> images) throws IOException {
+        List<String> imageUrls = new ArrayList<>();
+
+        for(var image : images) {
+            String uploadedImageUrl = s3ImageUploader.upload(image, PRODUCT_IMAGE_DIR);
+            imageUrls.add(uploadedImageUrl);
+        }
+
+        return imageUrls;
     }
 }
