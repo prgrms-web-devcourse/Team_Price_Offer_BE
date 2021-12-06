0b04922a4007b8515910614b7446033fe1aa52b3
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-06 16:15:37 +0900

    feat: schema.sql 추가

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,5 +1,39 @@
+-- members schema
 DROP TABLE IF EXISTS members CASCADE;
 
 create table members (member_id bigint not null, login_id varchar(255), password varchar(255), primary key (member_id));
 
 alter table members add constraint UK_lq5wej6688i1bd6b5c11neptj unique (login_id);
+
+-- article schema
+DROP TABLE IF EXISTS article CASCADE;
+
+create table article (
+                         article_id bigint not null,
+                         category_code integer,
+                         content TEXT,
+                         created_date timestamp,
+                         like_count integer,
+                         main_image_url TEXT,
+                         modified_date timestamp,
+                         price integer,
+                         product_status_code integer,
+                         quantity integer,
+                         title varchar(255),
+                         trade_area varchar(255),
+                         trade_method_code integer,
+                         trade_status_code integer,
+                         view_count integer,
+                         writer_id bigint,
+                         primary key (article_id)
+)
+
+-- product_image schema
+DROP TABLE IF EXISTS article CASCADE;
+
+create table product_image (
+                               product_image_id bigint not null,
+                               image_url varchar(255),
+                               article_id bigint,
+                               primary key (product_image_id)
+)
