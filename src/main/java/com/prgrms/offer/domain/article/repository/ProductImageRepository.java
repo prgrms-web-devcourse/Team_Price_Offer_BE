af235d275df7078e51563b6695a12d3bcc00fb4c
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-06 16:18:53 +0900

    chore: repository에 사용한 메소드 명시

--- src/main/java/com/prgrms/offer/domain/article/repository/ProductImageRepository.java
+++ src/main/java/com/prgrms/offer/domain/article/repository/ProductImageRepository.java
@@ -4,4 +4,6 @@ import com.prgrms.offer.domain.article.model.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    @Override
+    <S extends ProductImage> S save(S entity);
 }
