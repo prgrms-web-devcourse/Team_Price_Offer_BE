da8e6159540f3a6ce51597384a2df519fb80aefc
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-06 16:20:02 +0900

    fix: alter table 삭제

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -3,8 +3,6 @@ DROP TABLE IF EXISTS members CASCADE;
 
 create table members (member_id bigint not null, login_id varchar(255), password varchar(255), primary key (member_id));
 
-alter table members add constraint UK_lq5wej6688i1bd6b5c11neptj unique (login_id);
-
 -- article schema
 DROP TABLE IF EXISTS article CASCADE;
 
