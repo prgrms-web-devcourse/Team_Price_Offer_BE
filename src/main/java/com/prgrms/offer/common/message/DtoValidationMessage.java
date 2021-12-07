242aaeb3ff48577035012124a099a43b00c8a1a5
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-07 15:43:40 +0900

    feat: dto fail message 구체화

--- src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
+++ src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
@@ -1,7 +1,9 @@
 package com.prgrms.offer.common.message;
 
 public class DtoValidationMessage {
-    public static final String INVALID_POST_TITLE = "게시글 제목이 유효하지 않습니다.";
+    public static final String INVALID_TITLE_LENGTH = "게시글 제목길이가 올바르지 않습니다.";
     public static final String INVALID_TRADE_AREA = "거래지역이 올바르지 않습니다.";
-    public static final String INVALID_CODE = "유효하지 않은 trade status code 입니다.";
+    public static final String INVALID_CODE = "유효하지 않은 code입니다.";
+    public static final String INVALID_PRICE = "유효하지 않은 가격입니다.";
+    public static final String INVALID_CONTENT_LENGTH = "게시글 내용은 최소 5자 최대 2000자 이하여야 합니다.";
 }
