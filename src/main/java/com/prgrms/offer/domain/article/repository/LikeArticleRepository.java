94de9fd589dacc4958e1eb690f86a25150f75056
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-08 16:01:12 +0900

    feat: like article repository 생성

---
+++ src/main/java/com/prgrms/offer/domain/article/repository/LikeArticleRepository.java
@@ -0,0 +1,7 @@
+package com.prgrms.offer.domain.article.repository;
+
+import com.prgrms.offer.domain.article.model.entity.LikeArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeArticleRepository extends JpaRepository<LikeArticle, Long> {
+}
