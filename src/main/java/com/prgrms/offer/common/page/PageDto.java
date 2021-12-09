07bc5308839ba375d965f7f57e40aae0ddf216a3
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-09 18:55:05 +0900

    chore: page dto 필드명 수정

--- src/main/java/com/prgrms/offer/common/page/PageDto.java
+++ src/main/java/com/prgrms/offer/common/page/PageDto.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PageDto<T> {
 
-    private T data;
+    private T contents;
     private PageInfo pageInfo;
 
-    public static <T> PageDto<T> of(T data, PageInfo pageInfo) {
-        return new PageDto<>(data, pageInfo);
+    public static <T> PageDto<T> of(T contents, PageInfo pageInfo) {
+        return new PageDto<>(contents, pageInfo);
     }
 
 }
