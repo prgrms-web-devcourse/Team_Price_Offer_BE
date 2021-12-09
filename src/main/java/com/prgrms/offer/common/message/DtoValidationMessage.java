006a66add8c437fcadbe500154c2aebd703437ee
지훈 <goharrm@ajou.ac.kr>
2021-12-09 13:33:37 +0900

    Feat/#25 (#26)
    dto validation message 긴급 추가

--- src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
+++ src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
@@ -7,4 +7,5 @@ public class DtoValidationMessage {
     public static final String INVALID_PRICE = "유효하지 않은 가격입니다.";
     public static final String INVALID_CONTENT_LENGTH = "게시글 내용은 최소 5자 최대 2000자 이하여야 합니다.";
     public static final String EMPTY_IMAGE_URL = "image url이 비어있습니다.";
+    public static final String INVALID_PASSWORD_LENGTH = "비밀번호는 최소 8자 이상 최대 15자 이하어야 합니다.";
 }
