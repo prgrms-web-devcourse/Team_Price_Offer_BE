0923911163da214864c969c1d4ad058a0aa4418d
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-10 17:01:56 +0900

    chore: 사용한 메소드 명시

--- src/main/java/com/prgrms/offer/domain/offer/repository/OfferRepository.java
+++ src/main/java/com/prgrms/offer/domain/offer/repository/OfferRepository.java
@@ -4,4 +4,8 @@ import com.prgrms.offer.domain.offer.model.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    @Override
+    <S extends Offer> S save(S entity);
+
+    Long countByOffererIdAndArticleId(Long offererId, Long articleId);
 }
