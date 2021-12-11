60cc23ddfa59e15d62b254632ac5c76efa6f0e48
BeautterLife <pgh9801@gmail.com>
2021-12-11 21:05:01 +0900

    feat(search): 쪽지 Entity

---
+++ src/main/java/com/prgrms/offer/domain/message/model/entity/Message.java
@@ -0,0 +1,47 @@
+package com.prgrms.offer.domain.message.model.entity;
+
+import com.prgrms.offer.domain.member.model.entity.Member;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member receiver;
+
+    @Column(updatable = false)
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "messageRoomId")
+    private MessageRoom messageRoom;
+
+}
