e97c6e3624c795ae225a1500b4b9f9293a46f790
BeautterLife <pgh9801@gmail.com>
2021-12-12 00:26:41 +0900

    refactor(message): Dto생성 로직 이동(service->converter)

--- src/main/java/com/prgrms/offer/domain/message/service/MessageService.java
+++ src/main/java/com/prgrms/offer/domain/message/service/MessageService.java
@@ -11,15 +11,10 @@ import com.prgrms.offer.domain.message.model.entity.Message;
 import com.prgrms.offer.domain.message.model.entity.MessageRoom;
 import com.prgrms.offer.domain.message.repository.MessageRepository;
 import com.prgrms.offer.domain.message.repository.MessageRoomRepository;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +27,7 @@ public class MessageService {
     private final MessageRoomRepository messageRoomRepository;
     private final MemberRepository memberRepository;
     private final ArticleRepository articleRepository;
+    private final MessageConverter messageConverter;
     private final MessageRoomConverter messageRoomConverter;
 
     // 이미 보낸적 있는 사람에겐 채팅방 생성하지 않고 기존 채팅방 사용하기
@@ -56,32 +52,18 @@ public class MessageService {
             () -> createMessageRoom(sender, receiver, article, isMyMessageRoomExists)
         );
 
-
         MessageRoom offererMessageRoom = messageRoomRepository.findByMemberAndMessagePartnerAndArticle(
             receiver, sender, article).orElseGet(
             () -> createMessageRoom(receiver, sender, article, isOffererMessageRoomExits)
         );
 
-
-        Message myMessage = Message.builder()
-            .sender(sender)
-            .receiver(receiver)
-            .content(content)
-            .createdDate(LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS))
-            .messageRoom(myMessageRoom)
-            .build();
-
-        Message offererMessage = Message.builder()
-            .sender(sender)
-            .receiver(receiver)
-            .content(content)
-            .createdDate(LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS))
-            .messageRoom(offererMessageRoom)
-            .build();
+        Message myMessage = messageConverter.createMessage(sender, receiver, content,
+            myMessageRoom);
+        Message offererMessage = messageConverter.createMessage(sender, receiver, content,
+            offererMessageRoom);
 
         messageRepository.save(myMessage);
         messageRepository.save(offererMessage);
-
     }
 
     @Transactional
@@ -95,27 +77,17 @@ public class MessageService {
             messageRoom -> messageRepository.findTop1ByMessageRoomOrderByCreatedDateDesc(
                 messageRoom)).collect(Collectors.toList());
 
-        List<MessageRoomResponse> messageRoomResponseList = new ArrayList<>();
+        Page<MessageRoomResponse> messageRoomResponsePage =
+            messageRoomConverter.toMessageRoomResponsePage(messageRoomList, messageList, pageable);
 
-        Iterator<MessageRoom> messageRoomIterator = messageRoomList.listIterator();
-        Iterator<Message> messageIterator = messageList.listIterator();
-
-        while (messageRoomIterator.hasNext()) {
-            messageRoomResponseList.add(
-                messageRoomConverter.toMessageRoomResponse(messageRoomIterator.next(),
-                    messageIterator.next()));
-        }
-
-        final int start = (int)pageable.getOffset();
-        final int end = Math.min((start + pageable.getPageSize()), messageRoomResponseList.size());
-        final Page<MessageRoomResponse> page = new PageImpl<>(messageRoomResponseList.subList(start, end), pageable, messageRoomResponseList.size());
-
-        return page;
-
+        return messageRoomResponsePage;
     }
 
-    public MessageRoom createMessageRoom(Member member1, Member member2, Article article, Boolean isMessageRoomExists) {
-        if (!isMessageRoomExists) isMessageRoomExists = !isMessageRoomExists;
+    public MessageRoom createMessageRoom(Member member1, Member member2, Article article,
+        Boolean isMessageRoomExists) {
+        if (!isMessageRoomExists) {
+            isMessageRoomExists = !isMessageRoomExists;
+        }
         return messageRoomRepository.save(new MessageRoom(member1, member2, article));
     }
 
