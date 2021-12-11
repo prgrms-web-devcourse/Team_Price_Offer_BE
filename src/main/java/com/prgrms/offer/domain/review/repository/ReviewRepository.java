7e7e8c9b015bfb47bb0c3b7489295d11a3e92517
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-11 20:32:55 +0900

    chore: 사용한 메소드 명시

--- src/main/java/com/prgrms/offer/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/prgrms/offer/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.prgrms.offer.domain.review.repository;
 
+import com.prgrms.offer.domain.article.model.entity.Article;
+import com.prgrms.offer.domain.member.model.entity.Member;
 import com.prgrms.offer.domain.review.model.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Override
+    <S extends Review> S save(S entity);
+
+    boolean existsByFromMemberAndArticle(Member fromMember, Article article);
 }
