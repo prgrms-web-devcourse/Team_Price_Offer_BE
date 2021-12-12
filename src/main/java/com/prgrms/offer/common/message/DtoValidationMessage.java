85bfc73164dfe859a34ee195ccd61f6b70d5a63e
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-12 17:31:01 +0900

    chore: response message 내용 일부 수정

--- src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
+++ src/main/java/com/prgrms/offer/common/message/DtoValidationMessage.java
@@ -5,7 +5,7 @@ public class DtoValidationMessage {
     public static final String INVALID_TRADE_AREA = "거래지역이 올바르지 않습니다.";
     public static final String INVALID_CODE = "유효하지 않은 code입니다.";
     public static final String INVALID_PRICE = "유효하지 않은 가격입니다.";
-    public static final String INVALID_CONTENT_LENGTH = "게시글 내용은 최소 5자 최대 2000자 이하여야 합니다.";
+    public static final String INVALID_CONTENT_LENGTH = "내용은 최소 5자 최대 2000자 이하여야 합니다.";
     public static final String EMPTY_IMAGE_URL = "image url이 비어있습니다.";
     public static final String INVALID_PASSWORD_LENGTH = "비밀번호는 최소 8자 이상 최대 15자 이하어야 합니다.";
     public static final String INVALID_OFFER_PRICE = "제안 가격이 올바르지 않습니다.";
