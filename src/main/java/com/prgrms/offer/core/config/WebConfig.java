223162c00e54874de866ee3c2cf4f00a01db0c9d
awesomeo184 <awesomeo184@gmail.com>
2021-12-15 03:02:31 +0900

    feat: cors mapping

--- src/main/java/com/prgrms/offer/core/config/WebConfig.java
+++ src/main/java/com/prgrms/offer/core/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://offer-kohl.vercel.app")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
