fae7c9f0a2867e1220560ba20c7211d7c9740286
BeautterLife <pgh9801@gmail.com>
2021-12-15 01:36:38 +0900

    feat(message): 채팅방내 쪽지 전송, 채팅방 내역 조회

--- src/main/java/com/prgrms/offer/domain/message/service/MessageService.java
+++ src/main/java/com/prgrms/offer/domain/message/service/MessageService.java
@@ -6,17 +6,25 @@ import com.prgrms.offer.domain.article.model.entity.Article;
 import com.prgrms.offer.domain.article.repository.ArticleRepository;
 import com.prgrms.offer.domain.member.model.entity.Member;
 import com.prgrms.offer.domain.member.repository.MemberRepository;
+import com.prgrms.offer.domain.message.model.dto.MessageContentResponse;
+import com.prgrms.offer.domain.message.model.dto.MessageRequest;
+import com.prgrms.offer.domain.message.model.dto.MessageContentAndRoomInfo;
+import com.prgrms.offer.domain.message.model.dto.MessageRoomInfoResponse;
 import com.prgrms.offer.domain.message.model.dto.MessageRoomResponse;
+import com.prgrms.offer.domain.message.model.dto.OutgoingMessageResponse;
 import com.prgrms.offer.domain.message.model.entity.Message;
 import com.prgrms.offer.domain.message.model.entity.MessageRoom;
 import com.prgrms.offer.domain.message.repository.MessageRepository;
 import com.prgrms.offer.domain.message.repository.MessageRoomRepository;
+import com.prgrms.offer.domain.offer.model.entity.Offer;
+import com.prgrms.offer.domain.offer.repository.OfferRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
@@ -29,10 +37,13 @@ public class MessageService {
     private final ArticleRepository articleRepository;
     private final MessageConverter messageConverter;
     private final MessageRoomConverter messageRoomConverter;
+    private final OfferRepository offerRepository;
 
     // 이미 보낸적 있는 사람에겐 채팅방 생성하지 않고 기존 채팅방 사용하기
     @Transactional
-    public void sendFirstMessageToOfferer(long receiverId, String senderLoginId, long articleId,
+    public void sendMessageToOffererOnclickMessageButton(long receiverId, String senderLoginId,
+        long articleId,
+        long offerId,
         String content) {
 
         Member receiver = memberRepository.findById(receiverId)
@@ -44,28 +55,35 @@ public class MessageService {
         Article article = articleRepository.findById(articleId)
             .orElseThrow(() -> new BusinessException(ResponseMessage.ARTICLE_NOT_FOUND));
 
+        Offer offer = offerRepository.findById(offerId)
+            .orElseThrow(() -> new BusinessException(ResponseMessage.OFFER_NOT_FOUND));
+
         Boolean isMyMessageRoomExists = true;
         Boolean isOffererMessageRoomExits = true;
 
+        // 쪽지버튼 눌러서 보낼때(최초 전송 이후)
+        // messageRoom 찾아오는 쿼리메소드 간소화하기
+
         MessageRoom myMessageRoom = messageRoomRepository.findByMemberAndMessagePartnerAndArticle(
             sender, receiver, article).orElseGet(
-            () -> createMessageRoom(sender, receiver, article, isMyMessageRoomExists)
+            () -> createMessageRoom(sender, receiver, article, isMyMessageRoomExists, offer)
         );
 
         MessageRoom offererMessageRoom = messageRoomRepository.findByMemberAndMessagePartnerAndArticle(
             receiver, sender, article).orElseGet(
-            () -> createMessageRoom(receiver, sender, article, isOffererMessageRoomExits)
+            () -> createMessageRoom(receiver, sender, article, isOffererMessageRoomExits, offer)
         );
 
-        Message myMessage = messageConverter.createMessage(sender, receiver, content,
+        Message myMessage = messageConverter.createMessage(true, content,
             myMessageRoom);
-        Message offererMessage = messageConverter.createMessage(sender, receiver, content,
+        Message offererMessage = messageConverter.createMessage(false, content,
             offererMessageRoom);
 
         messageRepository.save(myMessage);
         messageRepository.save(offererMessage);
     }
 
+    // 쪽지함 가져오기
     @Transactional
     public Page<MessageRoomResponse> getMessageBox(String loginId, Pageable pageable) {
         Member me = memberRepository.findByPrincipal(loginId).get();
@@ -83,12 +101,88 @@ public class MessageService {
         return messageRoomResponsePage;
     }
 
+    // 대화방에서 메시지 전송
+    @Transactional
+    public OutgoingMessageResponse sendMessage(
+        long messageRoomId,
+        MessageRequest messageRequest,
+        String loginId) {
+        // 내가 대화방을 나간 상황
+        MessageRoom myMessageRoom = messageRoomRepository.findById(messageRoomId)
+            .orElseThrow(() -> new BusinessException(ResponseMessage.EXITED_MESSAGE_ROOM));
+
+        Member me = memberRepository.findByPrincipal(loginId)
+            .orElseThrow(() -> new BusinessException(ResponseMessage.MEMBER_NOT_FOUND));
+
+        // 다른 멤버의 대화방에 접근한 경우
+        if (myMessageRoom.getMember().getId() != me.getId()) {
+            throw new BusinessException(ResponseMessage.PERMISSION_DENIED);
+        }
+
+        // 상대방이 대화방을 나간 상황
+        Member messagePartner = myMessageRoom.getMessagePartner();
+
+        if (messagePartner == null) throw new BusinessException(ResponseMessage.MEMBER_NOT_FOUND);
+
+        MessageRoom receiverMessageRoom = messageRoomRepository.findByMemberAndOffer(
+                messagePartner, myMessageRoom.getOffer())
+            .orElseThrow(
+                () -> new BusinessException(ResponseMessage.MESSAGE_PARTNER_EXITED_MESSAGE_ROOM));
+
+//        receiverMessageRoom으로 receiver 찾기 vs myMessageRoom에서 messagePartner 찾기
+//        Member receiver = memberRepository.findById(receiverMessageRoom.getMember().getId())
+//            .orElseThrow(() -> new BusinessException(ResponseMessage.MEMBER_NOT_FOUND));
+
+        String content = messageRequest.getContent();
+
+        Message myMessage = messageConverter.createMessage(true, content,
+            myMessageRoom);
+        Message receiverMessage = messageConverter.createMessage(false, content,
+            receiverMessageRoom);
+
+        messageRepository.save(myMessage);
+        messageRepository.save(receiverMessage);
+
+        return messageConverter.toOutgoingMessageResponse(myMessage.getContent(),
+            myMessage.getCreatedDate());
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<MessageContentResponse> getMessageRoomContents(long messageRoomId, String loginId,Pageable pageable) {
+
+        MessageRoom myMessageRoom = messageRoomRepository.findById(messageRoomId)
+            .orElseThrow(() -> new BusinessException(ResponseMessage.MESSAGE_ROOM_NOT_FOUND));
+
+        Offer offer = myMessageRoom.getOffer();
+
+        List<Message> messageList = messageRepository.findByMessageRoom(myMessageRoom, pageable);
+
+        return messageConverter.toMessageContentResponsePage(messageList, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public MessageRoomInfoResponse getMessageRoomInfo(long messageRoomId, String loginId) {
+
+        MessageRoom myMessageRoom = messageRoomRepository.findById(messageRoomId)
+            .orElseThrow(() -> new BusinessException(ResponseMessage.MESSAGE_ROOM_NOT_FOUND));
+
+        Member messagePartner = myMessageRoom.getMessagePartner();
+
+        Offer offer = myMessageRoom.getOffer();
+        Article article = myMessageRoom.getArticle();
+
+        return messageConverter.toMessageRoomInfoResponse(messagePartner, article, offer);
+    }
+
+    @Transactional(propagation = Propagation.MANDATORY)
     public MessageRoom createMessageRoom(Member member1, Member member2, Article article,
-        Boolean isMessageRoomExists) {
+        Boolean isMessageRoomExists, Offer offer) {
         if (!isMessageRoomExists) {
             isMessageRoomExists = !isMessageRoomExists;
         }
-        return messageRoomRepository.save(new MessageRoom(member1, member2, article));
+        return messageRoomRepository.save(new MessageRoom(member1, member2, article, offer));
     }
+
 
 }
