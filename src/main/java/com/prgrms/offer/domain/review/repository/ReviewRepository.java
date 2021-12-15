e73674bfb6b97f795b828bb24c5fd22a39ee627d
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-15 18:42:56 +0900

    chore: 불필요한 import 문 제거

--- src/main/java/com/prgrms/offer/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/prgrms/offer/domain/review/repository/ReviewRepository.java
@@ -6,7 +6,6 @@ import com.prgrms.offer.domain.review.model.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
