a1c0a7c02f1a78ddb91968e8f2d35d239fa9ab68
BeautterLife <pgh9801@gmail.com>
2021-12-15 13:59:41 +0900

    fix(message): 응답 필드 누락 -> getter 추가

--- src/main/java/com/prgrms/offer/domain/message/model/dto/OutgoingMessageResponse.java
+++ src/main/java/com/prgrms/offer/domain/message/model/dto/OutgoingMessageResponse.java
@@ -3,7 +3,9 @@ package com.prgrms.offer.domain.message.model.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class OutgoingMessageResponse {
 
