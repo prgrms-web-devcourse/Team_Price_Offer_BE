77cbc6c56e727f3a1dae534c17a142af21ad4ea9
BeautterLife <pgh9801@gmail.com>
2021-12-16 21:44:18 +0900

    fix(message): 필드 검증 annotation 추가

--- src/main/java/com/prgrms/offer/domain/message/model/dto/MessageRequest.java
+++ src/main/java/com/prgrms/offer/domain/message/model/dto/MessageRequest.java
@@ -1,12 +1,13 @@
 package com.prgrms.offer.domain.message.model.dto;
 
 import com.prgrms.offer.common.message.DtoValidationMessage;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class MessageRequest {
 
-    @Size(min = 1, max = 4000, message = DtoValidationMessage.INVALID_CONTENT_LENGTH)
+    @NotNull(message = DtoValidationMessage.IVALID_MESSAGE_LENGTH)
+    @Size(min = 1, max = 100, message = DtoValidationMessage.IVALID_MESSAGE_LENGTH)
     private String content;
 
     public String getContent() {
